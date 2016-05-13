31866c133b53e30520d1b6407263895fd7a3d669
alsxilva <ale10fut@hotmail.com>
2016-05-13 04:57:40 +0000

    Archivos finales

--- Cliente.java
+++ Cliente.java
@@ -5,20 +5,38 @@ import java.util.*;
 public class Cliente{
 
 	public static void main(String args[]) throws UnknownHostException{
-
+		
 
 	try{
-		
-		Socket sock = new Socket("127.0.0.1", 8080);
-		Scanner in = new Scanner(sock.getInputStream());	
-		PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+		Socket cliente = new Socket("127.0.0.1", 8080);
+		Scanner in = new Scanner(cliente.getInputStream());	
+		PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
+		DataOutputStream salida = new DataOutputStream(cliente.getOutputStream());
 
 		out.println("get index.html");
 		System.out.println(in.nextLine());
 		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+
 		Scanner numero = new Scanner(System.in);
-  		float num1= numero.nextInt(); 
-		float num2= numero.nextInt();
+  		int num1= numero.nextInt(); 
+  		System.out.println("num1"+num1);
+  		
+  		Scanner numeo = new Scanner(System.in);
+		int num2= numeo.nextInt();
+		System.out.println("num2"+num2);
+
+		do{
+			salida.writeInt(num1);
+			salida.writeInt(num2);		
+		}while (num1!=0);
+
+		System.out.println(in.nextLine());
+		int operador= numero.nextInt(); salida.writeInt(operador);
+		System.out.println("numq1"+num1+"num2"+num2+"operador"+operador);
+		
+
+		System.out.println(in.nextLine());
 
 	}catch(IOException e){
 		System.out.println("No hay flujo...");
--- Servidor.java
+++ Servidor.java
@@ -5,17 +5,38 @@ import java.util.*;
 public class Servidor{
 
 	public static void main(String args[]) throws UnknownHostException{
-
-
+		
 	try{
-		ServerSocket server = new ServerSocket(8080);
 		
 		while(true){
-
+			ServerSocket server = new ServerSocket(8080);
 			Socket cliente = server.accept();
 			PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+			DataInputStream entrada =new DataInputStream(cliente.getInputStream());
+
 			System.out.println("Usuario conectado");
-			salida.println("Bienvenido\nOperadores +, -, * y /. Introduce dos numeros a operar:");
+			salida.println("Bienvenido\nOperadores +, -, * y /. \nIntroduce dos numeros a operar separados por un espacio:");
+			salida.println("Operacion: \t1. '+' \t2. '-' \t3. 'x'\t4. '/'");
+
+			int num1=  entrada.readInt(); 
+			int num2= entrada.readInt();
+			int operador= entrada.readInt();
+			System.out.println("num1 "+num1+"\tnum2 "+num2+"\toperador"+operador);
+				switch(operador){
+					case 1: int resultado=num1+num2;
+					break;
+					case 2:resultado=num1-num2;
+					break;
+					case 3:resultado=num1*num2;
+					break;
+					case 4:resultado=num1/num2;
+					break;
+					default: System.out.println("\t\t\t\tINTRODUCE UNA OPCION VALIDA\n\n\n\n");
+					break;	
+
+				}
+			//salida.println("Resultado:" + resultado);
+
 		}
 
 	}catch(IOException e){
